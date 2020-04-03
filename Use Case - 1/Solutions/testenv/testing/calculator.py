268050033d5e202a37c58d2f1e6a599773331feb
Prathmesh Maurya <prathmeshmaurya@gmail.com>
2020-04-03 10:34:18 +0530

    written test files

---
+++ Use Case - 1/Solutions/testenv/testing/calculator.py
@@ -0,0 +1,18 @@
+def add(a,b):
+	return a+b
+
+def subtract(a,b):
+	return a-b
+
+def multiply(a,b):
+	return a*b
+
+def divide(a,b):
+	a/b
+
+def maximum(a,b):
+	return a if a>= b else b
+
+def minimum(a,b):
+	return a if a<= b else b
+
---
+++ Use Case - 1/Solutions/testenv/testing/test_calculator.py
@@ -0,0 +1,31 @@
+
+import pytest
+from . import calculator
+
+NUMBER_1 = 3.0
+NUMBER_2 = 2.0
+
+
+def test_add():
+    value = calculator.add(NUMBER_1, NUMBER_2)
+    assert value == 5.0
+
+
+def test_subtract():
+    value = calculator.subtract(NUMBER_1, NUMBER_2)
+    assert value == 1.0
+
+
+def test_subtract_negative():
+    value = calculator.subtract(NUMBER_2, NUMBER_1)
+    assert value == -1.0
+
+
+def test_multiply():
+    value = calculator.multiply(NUMBER_1, NUMBER_2)
+    assert value == 6.0
+
+
+def test_divide():
+    value = calculator.divide(NUMBER_1, NUMBER_2)
+    assert value == 1.5
